5485a35b6486894f5b2d0b6abbac6d7095c8ba94
Jack VanDrunen <jack@fallingduck.net>
2015-11-27 00:49:01 -0800

    Added encoded address parsing and implemented part of MAIL and RCPT on the SMTP server

--- unstamp/mail_smtp_server.py
+++ unstamp/mail_smtp_server.py
@@ -38,6 +38,33 @@ def _parse_request(request):
     request = request.strip()
 
 
+def parse_address(encoded):
+    address = []
+    encoded = encoded.strip()
+    if not encoded or encoded[0] != '<':
+        return
+    inside_quotes = False
+    i = 1
+    if encoded[i] == '@':
+        while encoded[i] != ':':
+            i += 1
+        i += 1
+    while True:
+        if i >= len(encoded):
+            return  # Address is not properly terminated with '>'
+        char = encoded[i]
+        if char == '\\':
+            i += 1
+            address.append(encoded[i])
+        elif char == '"':
+            inside_quotes = not inside_quotes
+        elif char == '>' and not inside_quotes:
+            return ''.join(address)
+        else:
+            address.append(char)
+        i += 1
+
+
 def _accept(fp, host, port):
 
     writeline(fp, '220 {0} ESMTP'.format(_hostname))
@@ -61,12 +88,28 @@ def _accept(fp, host, port):
         verb, parameter = _parse_request(fp.readline())
 
         if verb == 'MAIL':
+            parameter = parameter.lower()
+            if parameter[:5] != 'from:':
+                writeline('500 No Address Given')
+                continue
+            mailfrom = parse_address(parameter[5:])
+            if not mailfrom:
+                writeline('501 Malformatted Address')
+                continue
+            envelope.FROM = mailfrom
             envelope.RECIPIENTS = []
-            # TODO
 
         elif verb == 'RCPT':
             if not envelope.FROM:
                 writeline(fp, '503 Need MAIL Before RCPT')
+                continue
+            parameter = parameter.lower()
+            if parameter[:3] != 'to:':
+                writeline('500 No Address Given')
+                continue
+            rcptto = parse_address(parameter[3:])
+            if rcptto is None:
+                writeline('501 Malformatted Address')
                 continue
             # TODO
 
